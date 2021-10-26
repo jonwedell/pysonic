fc96f8139b65acfe3ab15db94a737dbf0069e5fb
jon@jonwedell.com <f6ac4ceae0ad45089108fb1ed28d28b441a0866a>
2021-10-26 10:12:08 -0500

    Update the lyrics-getter to work with the changes to the Genius results page, and to use the search API in a way that yields better results.

--- pysonic/lyrics.py
+++ pysonic/lyrics.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 
 import os
+from typing import Optional
+
 import requests
 from bs4 import BeautifulSoup
 
-try:
-    user_home_path = os.path.abspath(os.path.join(os.path.expanduser("~"), ".pysonic/"))
-    token = open(os.path.join(user_home_path, "genius_api_key"), "r").read()
-except IOError:
-    token = None
 base_url = "https://api.genius.com"
-headers = {'Authorization': 'Bearer %s' % token}
+
+
+def get_token() -> Optional[str]:
+    user_home_path = os.path.abspath(os.path.join(os.path.expanduser("~"), ".pysonic/"))
+
+    try:
+        with open(os.path.join(user_home_path, "genius_api_key"), "r") as token_file:
+            token = token_file.read()
+    except (IOError, FileNotFoundError):
+        token = input("Please enter your Genius API token to use this feature (or press enter to cancel): ").strip()
+        if not token:
+            return None
+        with open(os.path.join(user_home_path, "genius_api_key"), "w") as token_file:
+            token_file.write(token)
+
+    return token
 
 
 def _lyrics_from_song_api_path(song_api_path):
     song_url = base_url + song_api_path
-    response = requests.get(song_url, headers=headers)
+    response = requests.get(song_url, headers={'Authorization': f'Bearer {get_token()}'})
     json = response.json()
     path = json["response"]["song"]["path"]
 
@@ -27,19 +39,26 @@ def _lyrics_from_song_api_path(song_api_path):
     # Remove script tags that they put in the middle of the lyrics
     [h.extract() for h in html('script')]
 
-    return html.find("div", class_="lyrics").get_text()
+    divs = html.findAll("div")
+    for div in divs:
+        classes = div.get('class')
+        if classes:
+            for one_class in classes:
+                if one_class.startswith('Lyrics__Container'):
+                    return div.get_text(separator="\n")
+
+    return 'Got a song response, but didn\'t find any lyircs.'
 
 
 def get_lyrics(song_title, artist_name):
     """ Return the lyrics for a given song and artist. """
 
-    if not token:
-        raise ValueError("Please put your Genius API token in the file "
-                         "'genius_api_key' and restart in order to fetch lyrics.")
-
     search_url = base_url + "/search"
     data = {'q': song_title + " " + artist_name}
-    response = requests.get(search_url, data=data, headers=headers)
+    token = get_token()
+    if not token:
+        return None, None, None
+    response = requests.get(search_url, params=data, headers={'Authorization': f'Bearer {token}'})
     json = response.json()
 
     # The first hit is the best?
